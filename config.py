b36f5a6fb0d85ffba20d0eaac0b551ea7acb349c
Gitphin <alek.holiman@gmail.com>
2024-09-08 00:52:10 -0500

    Added some more functionality in config

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ import os
 import pathlib
 
 class Config:
-    def __init__(self, ext_path='types_config.json', name_for_folder="SortedFolder"):
+    def __init__(self, ext_path='types_config.json',):
         self.ext_path = ext_path
-        self.name_for_folder = name_for_folder
-        self.main_folder_path = pathlib.Path.home() / 'Desktop' / 'Things' / self.name_for_folder
+        # Init file extensions and tags
 
-        # Init file extensions
         self.file_extensions = {
             "Images": ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'tiff'],
             "Videos": ['mp4', 'mkv', 'mov', 'avi', 'flv', 'wmv'],
@@ -17,9 +15,45 @@ class Config:
             "Misc": []
         }
 
-    def update_name(self, new):
-        self.name_for_folder = new
+        self.pref = {
+            "YEAR": True,
+            "CATEGORY": True,
+            "TYPES": True,
+            "TAG": "-=-",
+            "NAME": "SortedFolder",
+            "MAIN": "Main",
+        }
+        self.name = self.pref["NAME"]
+        # WILL HAVE TO UPDATE THIS LATER
+        self.main_folder_path = pathlib.Path.home() / 'Desktop' / 'Things' / self.name
+        self.tag = self.pref["TAG"]
+        self.main = self.pref["MAIN"]
+        self.year = self.pref["YEAR"]
+        self.types = self.pref["TYPES"]
+        self.category = self.pref["CATEGORY"]
 
+    def update_name(self, new):
+        """Updates what wrapper folder is called"""
+        os.rename(self.main_folder_path, self.main_folder_path / pathlib.Path.home() / 'Desktop' / 'Things' / new )
+        self.pref["NAME"] = new
+        self.name = new
+
+    def update_main(self, new):
+        """Updates main folder name"""
+        os.rename(self.main_folder_path / self.main, self.main_folder_path / new)
+        self.pref["MAIN"] = new
+        self.main = new
+
+    def update_tag(self, new):
+        """Updates tag indicator"""
+        self.pref["TAG"] = new
+        self.tag = new
+    
+    def update_toggle(self, t):
+        """Updates toggle filters"""
+        self.pref[t] = False if self.pref[t] else True
+        self.t = False if self.t else True
+        
     def update_ext_hash(self):
         """Refresh current extension hash"""
         try:
@@ -39,7 +73,7 @@ class Config:
         else:
             print(f"Loaded default extensions in")
 
-    def new_folder_type(self, name, exts):
+    def new_category_type(self, name, exts):
         """Add new category/folder"""
         if name in self.file_extensions:
             print(f"Category '{name}' already exists!")
@@ -48,7 +82,7 @@ class Config:
             self.file_extensions[name].extend([ext for ext in exts if ext not in self.file_extensions[name]])
             self.update_ext_hash()
 
-    def del_folder_type(self, name):
+    def del_category_type(self, name):
         """Delete category and exts"""
         if name not in self.file_extensions:
             print(f"Category '{name}' does not exist!")
@@ -56,7 +90,7 @@ class Config:
             del self.file_extensions[name]
             self.update_ext_hash()
 
-    def rename_folder_type(self, name, new):
+    def rename_category_type(self, name, new):
         """Rename category by replacing old with a new one"""
         if new in self.file_extensions:
             print(f"Category '{name}' already exists!")
